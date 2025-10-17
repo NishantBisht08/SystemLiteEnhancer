2d3516ed5589f31e29394ba93b00c5fb3d78f276
Khushi Baliyan <168220690+Khushi-Baliyan@users.noreply.github.com>
2025-10-17 10:58:36 +0530

    Clean up code in round_robin.py
    Removed comments to streamline the code.

--- src/classical/round_robin.py
+++ src/classical/round_robin.py
@@ -9,7 +9,6 @@ def round_robin_scheduling(processes, time_quantum):
     i = 0
 
     while completed < n:
-        # Add all processes that have arrived by this time
         for p in processes:
             if p.arrival_time <= time and p not in ready_queue and remaining_burst[p.pid - 1] > 0:
                 ready_queue.append(p)
@@ -23,12 +22,10 @@ def round_robin_scheduling(processes, time_quantum):
         remaining_burst[current.pid - 1] -= exec_time
         time += exec_time
 
-        # Add newly arrived processes
         for p in processes:
             if p.arrival_time <= time and p not in ready_queue and remaining_burst[p.pid - 1] > 0:
                 ready_queue.append(p)
 
-        # If process completed
         if remaining_burst[current.pid - 1] == 0:
             current.completion_time = time
             current.turnaround_time = current.completion_time - current.arrival_time
