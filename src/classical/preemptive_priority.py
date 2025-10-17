a586b4270a76dfc5b9c0662f5135bed85d9b90af
Khushi Baliyan <168220690+Khushi-Baliyan@users.noreply.github.com>
2025-10-17 13:14:37 +0530

    Refactor preemptive_priority_scheduling function

--- src/classical/preemptive_priority.py
+++ src/classical/preemptive_priority.py
@@ -1,14 +1,12 @@
 from src.classical.process import Process
 
 def preemptive_priority_scheduling(processes):
-    # Sort by arrival time for tie-breaking
     processes.sort(key=lambda x: x.arrival_time)
     n = len(processes)
     completed = 0
     current_time = 0
     is_completed = [False] * n
 
-    # Make sure each process has remaining_time initialized
     for p in processes:
         p.remaining_time = p.burst_time
 
@@ -16,7 +14,6 @@ def preemptive_priority_scheduling(processes):
         index = -1
         highest_priority = float('inf')
 
-        # Find the process with highest priority among arrived and incomplete
         for i in range(n):
             if processes[i].arrival_time <= current_time and not is_completed[i] and processes[i].remaining_time > 0:
                 if processes[i].priority < highest_priority:
@@ -28,15 +25,12 @@ def preemptive_priority_scheduling(processes):
 
         if index != -1:
             process = processes[index]
-            # If process starts for the first time
             if process.start_time is None:
                 process.start_time = current_time
 
-            # Execute for 1 time unit (preemption possible)
             process.remaining_time -= 1
             current_time += 1
 
-            # If process finished
             if process.remaining_time == 0:
                 process.completion_time = current_time
                 process.turnaround_time = process.completion_time - process.arrival_time
@@ -44,8 +38,8 @@ def preemptive_priority_scheduling(processes):
                 is_completed[index] = True
                 completed += 1
         else:
-            # If no process has arrived yet or all are completed, just move time forward
             current_time += 1
 
     return processes
+
 
